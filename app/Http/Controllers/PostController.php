404e8c8776f479af5e14b00eae7983c3b6f9638a
AlbertoGuzman <2519160038jguzmang@gmail.com>
2022-05-07 21:45:07 -0500

    Comentarios y excepciones al controlador PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,61 +9,115 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function get_list_user()
     {
-        $users = Http::get('https://jsonplaceholder.typicode.com/users')->json();
+        try {
+            $users = Http::get('https://jsonplaceholder.typicode.com/users')->json();
 
-        $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
-        $collectionPost = collect($posts);
-        $countPosts = $collectionPost->countBy('userId');
+            $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+            $collectionPost = collect($posts);
+            $countPosts = $collectionPost->countBy('userId');
 
-        $albums = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
-        $collectionAlbums = collect($albums);
-        $countAlbums = $collectionAlbums->countBy('userId');
+            $albums = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
+            $collectionAlbums = collect($albums);
+            $countAlbums = $collectionAlbums->countBy('userId');
 
-        return view('index', [
-            'users' => $users,
-            'countPosts' => $countPosts,
-            'countAlbums' => $countAlbums
-        ]);
+            $ip = empty($_SERVER["REMOTE_ADDR"]) ? "Desconocida" : $_SERVER["REMOTE_ADDR"];
+            \Log::info('User request made ' . $ip);
+            \Log::info('Posts request made ' . $ip);
+            \Log::info('Albums request made ' . $ip);
+            return view('index', [
+                'users' => $users,
+                'countPosts' => $countPosts,
+                'countAlbums' => $countAlbums
+            ]);
+        } catch (\Exception $e) {
+            \Log::debug('Test var fails' . $e->getMessage());
+        }
     }
 
+    /**
+     * Display the specified resources.
+     *
+     * @param  int  $userId
+     * @return \Illuminate\Http\Response
+     */
     public function get_list_posts($userId)
     {
-        $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
-        $user = Http::get("https://jsonplaceholder.typicode.com/users/$userId")->json();
-        $collectionPosts = collect($posts)->where('userId', $userId);
-        return view('posts.index', [
-            'collectionPosts' => $collectionPosts,
-            'user' => $user
-        ]);
+        try {
+            $posts = Http::get('https://jsonplaceholder.typicode.com/posts')->json();
+            $user = Http::get("https://jsonplaceholder.typicode.com/users/$userId")->json();
+            $collectionPosts = collect($posts)->where('userId', $userId);
+
+            $ip = empty($_SERVER["REMOTE_ADDR"]) ? "Desconocida" : $_SERVER["REMOTE_ADDR"];
+            \Log::info('User request made ' . $ip);
+            \Log::info('Posts request made ' . $ip);
+            return view('posts.index', [
+                'collectionPosts' => $collectionPosts,
+                'user' => $user
+            ]);
+        } catch (\Exception $e) {
+            \Log::debug('Test var fails' . $e->getMessage());
+        }
     }
 
+    /**
+     * Display the specified resources.
+     *
+     * @param  int  $userId
+     * @return \Illuminate\Http\Response
+     */
     public function get_list_albums($userId)
     {
-        $albums = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
-        $user = Http::get("https://jsonplaceholder.typicode.com/users/$userId")->json();
-        $collectionAlbums = collect($albums)->where('userId', $userId);
+        try {
+            $albums = Http::get('https://jsonplaceholder.typicode.com/albums')->json();
+            $user = Http::get("https://jsonplaceholder.typicode.com/users/$userId")->json();
+            $collectionAlbums = collect($albums)->where('userId', $userId);
 
-        $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
-        $collectionPhotos = collect($photos);
-        $countPhotos = $collectionPhotos->countBy('albumId');
-        return view('albums.index', [
-            'collectionAlbums' => $collectionAlbums,
-            'countPhotos' => $countPhotos,
-            'user' => $user
-        ]);
+            $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
+            $collectionPhotos = collect($photos);
+            $countPhotos = $collectionPhotos->countBy('albumId');
+
+            $ip = empty($_SERVER["REMOTE_ADDR"]) ? "Desconocida" : $_SERVER["REMOTE_ADDR"];
+            \Log::info('User request made ' . $ip);
+            \Log::info('Albums request made ' . $ip);
+            return view('albums.index', [
+                'collectionAlbums' => $collectionAlbums,
+                'countPhotos' => $countPhotos,
+                'user' => $user
+            ]);
+        } catch (\Exception $e) {
+            \Log::debug('Test var fails' . $e->getMessage());
+        }
     }
 
+    /**
+     * Display the specified resources.
+     *
+     * @param  int  $albumId
+     * @return \Illuminate\Http\Response
+     */
     public function get_list_photos($albumId)
     {
-        $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
-        $album = Http::get("https://jsonplaceholder.typicode.com/albums/$albumId")->json();
-        $collectionPhotos = collect($photos)->where('albumId', $albumId);
+        try {
+            $photos = Http::get('https://jsonplaceholder.typicode.com/photos')->json();
+            $album = Http::get("https://jsonplaceholder.typicode.com/albums/$albumId")->json();
+            $collectionPhotos = collect($photos)->where('albumId', $albumId);
 
-        return view('albums.show', [
-            'collectionPhotos' => $collectionPhotos,
-            'album' => $album
-        ]);
+            $ip = empty($_SERVER["REMOTE_ADDR"]) ? "Desconocida" : $_SERVER["REMOTE_ADDR"];
+            \Log::info('Photos request made ' . $ip);
+            \Log::info('Albums request made ' . $ip);
+            return view('albums.show', [
+                'collectionPhotos' => $collectionPhotos,
+                'album' => $album
+            ]);
+        } catch (\Exception $e) {
+            \Log::debug('Test var fails' . $e->getMessage());
+        }
     }
 }
